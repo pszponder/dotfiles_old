fbae66096602e0fd03ee305ce88b9bf6d349fcca
pszponder <14116413+pszponder@users.noreply.github.com>
2023-11-29 00:02:31 -0700

    add directions for installing docker engine

--- install/.config/install/scripts/install_apps.sh
+++ install/.config/install/scripts/install_apps.sh
@@ -71,4 +71,38 @@ flatpak install flathub md.obsidian.Obsidian
 # ===================================
 # echo "Installing Bruno..."
 # nix-env -iA nixpkgs.bruno
-snap install bruno
+# snap install bruno
+
+# =========================================================
+# Install Docker Engine
+# https://docs.docker.com/engine/
+# https://docs.docker.com/engine/install/linux-postinstall/
+# =========================================================
+
+echo "Installing Docker Engine..."
+
+# Uninstall old versions
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker Engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Create the docker group.
+sudo groupadd docker
+
+# Add your user to the docker group (log out and back in if necessary)
+sudo usermod -aG docker $USER
