cc0e506782282825b64bf7e455fb9c9af0357a8c
Your Name <your.email@example.com>
2023-12-02 20:56:36 -0700

    update setup for rtx

--- install/.config/install/scripts/setup_rtx-polyglot.sh
+++ install/.config/install/scripts/setup_rtx-polyglot.sh
@@ -4,34 +4,38 @@
 if test ! $(which rtx); then
 	echo "Installing rtx..."
 	brew install rtx
+
+	# Install NodeJS
+	echo "Installing NodeJS..."
+	rtx install node
+	rtx use -g node
+
+	# Install BunJS
+	echo "Installing BunJS..."
+	rtx install bun
+	rtx use -g bun
+
+	# Install Golang
+	echo "Installing Golang..."
+	rtx install go
+	rtx use -g go
+
+	# install Python
+	echo "Installing Python..."
+	rtx install python
+	rtx use -g python
+
+	# Install Dotnet
+	echo "Installing Dotnet..."
+	rtx install dotnet
+	rtx use -g dotnet
+
+	# Install Rust
+	echo "Installing Rust..."
+	rtx install rust
+	rtx use -g rust
+
+else
+	echo "rtx is already installed, installing languages from ~/.config/rtx/config.toml ..."
+	rtx install
 fi
-
-# Install NodeJS
-echo "Installing NodeJS..."
-rtx install node
-rtx use -g node
-
-# Install BunJS
-echo "Installing BunJS..."
-rtx install bun
-rtx use -g bun
-
-# Install Golang
-echo "Installing Golang..."
-rtx install go
-rtx use -g go
-
-# install Python
-echo "Installing Python..."
-rtx install python
-rtx use -g python
-
-# Install Dotnet
-echo "Installing Dotnet..."
-rtx install dotnet
-rtx use -g dotnet
-
-# Install Rust
-echo "Installing Rust..."
-rtx install rust
-rtx use -g rust
