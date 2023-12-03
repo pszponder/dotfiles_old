e29ab35d56d42aa24230d3c1067e5809af09ab02
pszponder <14116413+pszponder@users.noreply.github.com>
2023-12-02 18:56:38 -0700

    update git config and setup script

--- install/.config/install/scripts/setup_git.sh
+++ install/.config/install/scripts/setup_git.sh
@@ -12,23 +12,6 @@ read -p "Enter your Git email address: " user_email
 git config --global user.name "$user_name"
 git config --global user.email "$user_email"
 
-# Additional Configurations
-git config --global core.editor nvim
-# git config --global core.editor "code --wait"
-git config --global core.autoCRLF input # Convert CRLF to LF on commit
-git config --global color.ui true # Enable colored output in Git CLI
-
-# Common configurations for git-delta
-# https://github.com/dandavison/delta
-git config --global core.pager "delta --dark"
-git config --global interactive.diffFilter "delta --color-only" # Use delta for diffs
-git config --global delta.syntax-theme "Dracula"
-git config --global delta.line-numbers true
-git config --global delta.side-by-side true
-git config --global delta.zero-width-emphasys true # Use background color instead of bold text
-git config --global delta.navigate true # Enable file navigation
-git config --global delta.decorate-auto true # Enable file decorations
-
 # Display the configuration for confirmation
 echo "Git global configuration updated:"
 git config --list --global
