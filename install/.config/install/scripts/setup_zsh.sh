0b1f0d95e6c07c4e0008c2153faf81a0d289d7c6
Your Name <your.email@example.com>
2023-12-02 23:34:32 -0700

    install zsh using homebrew

--- install/.config/install/scripts/setup_zsh.sh
+++ install/.config/install/scripts/setup_zsh.sh
@@ -7,8 +7,9 @@
 
 # Append zsh to /etc/shells (valid login shells)
 echo "Installing ZSH..."
-sudo apt install zsh
-command -v zsh | sudo tee -a /etc/shells
+# sudo apt install zsh
+brew install zsh
+command -v zsh | sudo tee -a /etc/shells # Append zsh to /etc/shells (valid login shells)
 
 # Change default Shell to ZSH
 sudo chsh -s $(which zsh) $USER
